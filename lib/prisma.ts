676f9cc04653d4341ba29d0a140eaaa5f529b9c4
eslamshaban060 <eslamshaban060@gmail.com>
2026-02-25 15:38:17 +0200

    solve the problem of prisma clinet

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,27 @@
-// import { PrismaClient } from "@prisma/client";
-import Prisma from "@prisma/client";
+// // import { PrismaClient } from "@prisma/client";
+// import Prisma from "@prisma/client";
 
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { Pool } from "pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-const adapter = new PrismaPg(pool);
+// const adapter = new PrismaPg(pool);
 
-// export const prisma = new PrismaClient({
+// // export const prisma = new PrismaClient({
+// //   adapter,
+// // });
+// export const prisma = new Prisma.PrismaClient({
 //   adapter,
 // });
-export const prisma = new Prisma.PrismaClient({
-  adapter,
-});
+
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/src/generated/prisma";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
